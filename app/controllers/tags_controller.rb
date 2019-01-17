08227d638278c83d5561fa7cbd7edcb5843e263e
niclausliu <e0323628@u.nus.edu>
2019-01-17 09:40:15 +0800

    solve tag index bugs

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,6 +1,10 @@
 class TagsController < ApplicationController
   def index
-    @tags = ActsAsTaggableOn::Tag.all
+    @tags = [];
+    @tags_all = ActsAsTaggableOn::Tag.all
+    @tags_all.each do |tag|
+	@tags << tag if current_user.tasks.tagged_with(tag.name) != []
+    end
   end
 
   def show
